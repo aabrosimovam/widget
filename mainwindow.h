95b6e4bc8da23ee1148d9c510aa4db3aef53d398
rita <pocyriteko@gmail.com>
2023-05-24 01:05:59 +0700

    task 2.7
    *Реализация "возведения числа в квадрат"

--- mainwindow.h
+++ mainwindow.h
@@ -1,5 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#include <QtGui>
+#include <QWidget>
+#include <QLabel>
+#include <QFrame>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QValidator>
+#include <QMainWindow>
 
 #include <QMainWindow>
 
@@ -7,16 +15,34 @@ namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
+class MainWindow:public QWidget // класс окна
 {
-    Q_OBJECT
-
+    Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+protected:
+    QTextCodec *codec;
+    QFrame *frame; // рамка
+    QLabel *inputLabel; // метка ввода
+    QLineEdit *inputEdit; // строчный редактор ввода
+    QLabel *outputLabel; // метка вывода
+    QLineEdit *outputEdit; // строчный редактор вывода
+    QPushButton *nextButton; // кнопка Следующее
+    QPushButton *exitButton; // кнопка Выход
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
+    MainWindow(QWidget *parent = 0); // конструктор
+public slots:
+    void begin(); // метод начальной настройки интерфейса
+    void calc(); // метод реализации вычислений
 };
+
+
+class StrValidator:public QValidator // класс компонента проверки ввода
+{
+public:
+    StrValidator(QObject *parent):QValidator(parent){}
+    virtual State validate(QString &str,int &pos)const
+    { return Acceptable;} // метод всегда принимает вводимую строку
+
+};
+
 
 #endif // MAINWINDOW_H
