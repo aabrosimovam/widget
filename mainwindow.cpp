145780d7cc367ca36620d635df7843e7efbe7d7a
rita <pocyriteko@gmail.com>
2023-05-24 01:09:21 +0700

    task 2.8
    *Счетчик по 1 и по 5

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,42 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "mainwindow.h" // подключение заголовочного файла MainWindow
+#include "ui_mainwindow.h" // подключение файла с реализацией пользовательского интерфейса
+#include <QVBoxLayout> // подключение класса QVBoxLayout для вертикального размещения виджетов
+#include <QPushButton> // подключение класса QPushButton для создания кнопок
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
 
-MainWindow::~MainWindow()
+
+MainWindow::MainWindow(QWidget *parent):QWidget(parent) // определение конструктора класса MainWindow
 {
-    delete ui;
+    codec = QTextCodec::codecForName("UTF-8"); // установка кодека для поддержки русского языка
+    this->setWindowTitle(codec->toUnicode("Счетчик")); // установка заголовка окна
+
+    label1 = new QLabel(codec->toUnicode("Cчет по 1"),this); // создание метки "Cчет по 1"
+    label2 = new QLabel(codec->toUnicode("Cчет по 5"),this); // создание метки "Cчет по 5"
+
+    edit1 = new Counter("0",this); // создание объекта класса Counter для счета по 1
+    edit2 = new Counter("0",this); // создание объекта класса Counter для счета по 5
+
+    calcbutton=new QPushButton("+1",this); // создание кнопки "+1"
+    exitbutton=new QPushButton(codec->toUnicode("Выход"),this); // создание кнопки "Выход"
+
+    QHBoxLayout *layout1 = new QHBoxLayout(); // создание горизонтального блока layout1
+    layout1->addWidget(label1); // добавление метки label1 в блок layout1
+    layout1->addWidget(label2); // добавление метки label2 в блок layout1
+
+    QHBoxLayout *layout2 = new QHBoxLayout(); // создание горизонтального блока layout2
+    layout2->addWidget(edit1); // добавление объекта edit1 в блок layout2
+    layout2->addWidget(edit2); // добавление объекта edit2 в блок layout2
+
+    QHBoxLayout *layout3 = new QHBoxLayout(); // создание горизонтального блока layout3
+    layout3->addWidget(calcbutton); // добавление кнопки calcbutton в блок layout3
+    layout3->addWidget(exitbutton); // добавление кнопки exitbutton в блок layout3
+
+    QVBoxLayout *layout4 = new QVBoxLayout(this); // создание вертикального блока layout4
+    layout4->addLayout(layout1); // добавление блока layout1 в блок layout4
+    layout4->addLayout(layout2); // добавление блока layout2 в блок layout4
+    layout4->addLayout(layout3); // добавление блока layout3 в блок layout4
+
+    connect(calcbutton, SIGNAL(clicked(bool)), edit1, SLOT(add_one())); // связь нажатия кнопки calcbutton со слотом add_one объекта edit1
+    connect(edit1, SIGNAL(tick_signal()), edit2, SLOT(add_one())); // связь сигнала tick_signal объекта edit1 со слотом add_one объекта edit2
+    connect(exitbutton, SIGNAL(clicked(bool)), this, SLOT(close())); // связь нажатия кнопки exitbutton со слотом close() окна
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,47 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtGui>
+#include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 
 namespace Ui {
-class MainWindow;
+    class MainWindow; // объявление класса MainWindow в пространстве имен Ui
 }
 
-class MainWindow : public QMainWindow
+class Counter:public QLineEdit // определение класса Counter, наследующего QLineEdit
 {
     Q_OBJECT
-
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    Counter(const QString & contents, QWidget *parent=0): // конструктор класса Counter
+    QLineEdit(contents,parent){} // вызов конструктора базового класса
+signals:
+    void tick_signal(); // объявление сигнала tick_signal
+public slots:
+    void add_one() // объявление слота add_one
+    {
+        QString str=text(); // получение текста из поля ввода
+        int r=str.toInt(); // преобразование текста в число
+        if (r!=0 && r%5 ==0) // генерация сигнала tick_signal, если число не равно 0 и делится на 5 без остатка
+            emit tick_signal();
+        r++;
+        str.setNum(r); // преобразование числа в строку
+        setText(str); // установка нового значения поля ввода
+    }
+};
 
-private:
-    Ui::MainWindow *ui;
+class MainWindow: public QWidget{ // определение класса MainWindow, наследующего QWidget
+    Q_OBJECT
+protected:
+    QTextCodec *codec; // указатель на объект QTextCodec для поддержки русского языка
+    QLabel *label1, *label2; // указатели на объекты QLabel для отображения текста
+    Counter *edit1, *edit2; // указатели на объекты Counter для ввода чисел
+    QPushButton *calcbutton; // указатель на объект QPushButton для кнопки "+1"
+    QPushButton *exitbutton; // указатель на объект QPushButton для кнопки "Выход"
+public:
+    MainWindow(QWidget *parent = 0); // конструктор класса MainWindow
 };
 
 #endif // MAINWINDOW_H
